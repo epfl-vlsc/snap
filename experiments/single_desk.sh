a223e4ec2f49d4616633cd384892d4b836f0779b
Sam Whitlock <sam.whitlock@epfl.ch>
2016-08-20 13:50:19 +0200

    fixing the script a bit

--- experiments/single_desk.sh
+++ experiments/single_desk.sh
@@ -2,17 +2,21 @@
 
 set -eu
 
-DIR=$(dirname $0)
+DIR=$(dirname "${BASH_SOURCE[0]}")
 num_threads=$1
 ref_index_path=/single_data/ref_index
 input_path=/single_data/ERR174324_1.fastq.gz
 output_path=/single_data/output.sam
 flush_path=$DIR/../../tf-align/util/flush-cache.sh
 
-make -C $DIR/..
-sudo $flush_path -p
+prep_dir() {
+    make -C $DIR/..
+    sudo $flush_path -p
+    parallel "cat {} >/dev/null" ::: $ref_index_path/*
+    [ -e $output_path] && rm $output_path
+}
 
-parallel "cat {} >/dev/null" ::: $ref_index_path/*
-[ -e $output_path] && rm $output_path
+prep_dir
 
-../snap-aligner single $ref_index_path -t $num_threads -compressedFastq $input_path -o $output_path & ; echo $! > pid && pidstat -hrdu -p `cat pid` 1 | sed '1d;/^[#]/{4,$d};/^[#]/s/^[#][ ]*//;/^$/d;s/^[ ]*//;s/[ ]\+/,/g' > pidstat_$num_threads.csv && rm pid
+../snap-aligner single $ref_index_path -t $num_threads -compressedFastq $input_path -o $output_path &
+echo $! > pid && pidstat -hrdu -p `cat pid` 1 | sed '1d;/^[#]/{4,$d};/^[#]/s/^[#][ ]*//;/^$/d;s/^[ ]*//;s/[ ]\+/,/g' > pidstat_$num_threads.csv && rm pid
